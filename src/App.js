f5ac9e5f99cbde4a3b5086b128ea4aea8f52268e
saswat <saswatranjan0602@gmail.com>
2023-07-09 22:51:21 +0530

    news-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,100 @@
-import './App.css';
+import "./App.css";
 
-import React, { Component } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <News pageSize={5} country="in" />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={9}
+                  country="us"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={9}
+                  country="us"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={9}
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={9}
+                  country="us"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={9}
+                  country="us"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={9}
+                  country="us"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={9}
+                  country="us"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
     );
   }
 }
-
-
 
 // 90c395b806384ca0b8b42958fc861b0d
